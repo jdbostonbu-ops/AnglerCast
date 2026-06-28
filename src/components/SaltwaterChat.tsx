0d0bc99c9cde7f00ffcb782260c86c8b527e98e7
Jacqueline Delgado <jdboston.bu@gmail.com>
2026-06-28 15:49:17 -0400

    GREEN 37.24: SaltwaterChat renders a labeled question input and a submit button

---
+++ src/components/SaltwaterChat.tsx
@@ -0,0 +1,7 @@
+export const SaltwaterChat = () => (
+  <form>
+    <label htmlFor="saltwater-chat-question">Question</label>
+    <input id="saltwater-chat-question" name="question" type="text" />
+    <button type="submit">Submit</button>
+  </form>
+);
